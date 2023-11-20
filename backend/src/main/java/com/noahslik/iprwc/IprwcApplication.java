bfe02bb7f1e650e506b409c5fcf94274370d0e6d
Noah Slik <noahslik99@gmail.com>
2023-11-20 14:36:01 +0100

    feat(backend): initialize spring project

---
+++ backend/src/main/java/com/noahslik/iprwc/IprwcApplication.java
@@ -0,0 +1,13 @@
+package com.noahslik.iprwc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprwcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IprwcApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/noahslik/iprwc/IprwcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noahslik.iprwc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IprwcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
